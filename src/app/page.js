22144ef00bb5dbc883888522cdf2551bd61d5693
netstermonir <monirhossain6163@gmail.com>
2024-01-21 00:07:34 +0600

    traffic

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import TrafficLight from "@/components/TrafficLight";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-center items-center bg-black">
+    <main className="flex min-h-screen pl-6 md:pl-0 justify-start md:justify-center items-center bg-black">
       <TrafficLight />
     </main>
   );
